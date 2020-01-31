e49252b366b65bce9e1e9e71512601347a57b660
Avery Vine <averyvine@icloud.com>
2020-01-30 19:02:19 -0500

    added sudoku

---
+++ sudoku/SudokuTest.java
@@ -0,0 +1,21 @@
+package sudoku; //No touchy
+
+/**
+ * Your goal is to ensure all row, columns, and boxes are valid and return true if they are, false otherwise.
+ * - A valid row has the numbers 1-9 with a frequency of once each
+ * - A valid col has the numbers 1-9 with a frequency of once each
+ * - A valid 3x3 box has the numbers 1-9 with a frequency of once each
+ *
+ * TO COMPILE:
+ * - Navigate to one level above the sudoku directory
+ * - javac sudoku/SudokuTest.java
+ * - java sudoku/Sudoku.java
+ */
+public class SudokuTest {
+    public static boolean isValid(int board[][]) {
+        /**
+         * Your code goes here!
+         */
+        return false;
+    }
+}
